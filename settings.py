4081f81e5433999ef9a1e772963f31328b1804dc
Chris Wanstrath <chris@ozmm.org>
2009-04-25 02:01:09 -0700

    more verbose settings file

--- settings.py
+++ settings.py
@@ -1,14 +1,21 @@
-# the bot only listens to one user.
-BLESSED_USER = "your_name"
+# create a local_settings.py and overwrite
+# these values:
 
-IRC_CHANNEL = "private_room"
-IRC_SERVER  = "irc.freenode.net"
-IRC_PORT    = 6667
-
+BLESSED_USER       = "your_name"  # the bot only listens to one user.
+IRC_CHANNEL        = "private_room"
 CAMPFIRE_SUBDOMAIN = "mycompany"
 CAMPFIRE_ROOM      = "The Good Room"
 CAMPFIRE_EMAIL     = "ircamp@gmail.com"
 CAMPFIRE_PASSWORD  = "1rc4mp"
+
+
+
+
+# you probably don't want anything below
+# this line in local_settings.py
+
+IRC_SERVER  = "irc.freenode.net"
+IRC_PORT    = 6667
 
 try:
     from local_settings import *
